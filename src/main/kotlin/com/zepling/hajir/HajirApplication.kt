ec07f39e322075997674585aba5da6715cb908e4
theanilpaudel <theanilpaudel@gmail.com>
2022-05-16 17:20:35 +0545

    updated firebase classpath

--- src/main/kotlin/com/zepling/hajir/HajirApplication.kt
+++ src/main/kotlin/com/zepling/hajir/HajirApplication.kt
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.Resource
 import org.springframework.core.io.ResourceLoader
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import java.io.FileInputStream
@@ -33,23 +34,23 @@ class BeanConfiguration{
     fun initializeFirebase(){
          try {
             println("initializing firebase bean")
-            /*val resource: Resource? =
+            val resource: Resource? =
                 resourceLoader?.getResource("classpath:firebase/hajir-10448-firebase-adminsdk-vw6rv-b6f712cf66.json")
             val serviceAccount = resource?.inputStream
             val credentials = GoogleCredentials.fromStream(serviceAccount)
             val options = FirebaseOptions.builder()
                 .setCredentials(credentials)
                 .build()
-            FirebaseApp.initializeApp(options)*/
+            FirebaseApp.initializeApp(options)
 
 
-             val serviceAccount = FileInputStream("/Users/anilpaudel/SpringProjects/hajir/src/main/resources/firebase/hajir-10448-firebase-adminsdk-vw6rv-b6f712cf66.json")
+             /*val serviceAccount = FileInputStream("/Users/anilpaudel/SpringProjects/hajir/src/main/resources/firebase/hajir-10448-firebase-adminsdk-vw6rv-b6f712cf66.json")
 
              val options = FirebaseOptions.builder()
                  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                  .build()
 
-             FirebaseApp.initializeApp(options)
+             FirebaseApp.initializeApp(options)*/
 
 
          } catch (e: IOException) {
