0d5531cc8b5706883e07f6cb87365f3d9b24c2c2
theanilpaudel <theanilpaudel@gmail.com>
2022-05-16 17:24:16 +0545

    updated firebase classpath

--- src/main/kotlin/com/zepling/hajir/HajirApplication.kt
+++ src/main/kotlin/com/zepling/hajir/HajirApplication.kt
@@ -22,7 +22,7 @@ import java.io.IOException
 class HajirApplication
 
 @Autowired
-var resourceLoader: ResourceLoader? = null
+lateinit var resourceLoader: ResourceLoader
 
 fun main(args: Array<String>) {
     runApplication<HajirApplication>(*args)
@@ -34,9 +34,9 @@ class BeanConfiguration{
     fun initializeFirebase(){
          try {
             println("initializing firebase bean")
-            val resource: Resource? =
-                resourceLoader?.getResource("classpath:firebase/hajir-10448-firebase-adminsdk-vw6rv-b6f712cf66.json")
-            val serviceAccount = resource?.inputStream
+            val resource: Resource =
+                resourceLoader.getResource("classpath:firebase/hajir-10448-firebase-adminsdk-vw6rv-b6f712cf66.json")
+            val serviceAccount = resource.inputStream
             val credentials = GoogleCredentials.fromStream(serviceAccount)
             val options = FirebaseOptions.builder()
                 .setCredentials(credentials)
