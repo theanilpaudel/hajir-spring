6c949d2530eb546fe7081af9d29c9ae6becc325e
theanilpaudel <theanilpaudel@gmail.com>
2022-05-16 15:08:04 +0545

    updated datetime in sheets

--- src/main/kotlin/com/zepling/hajir/utils/Exts.kt
+++ src/main/kotlin/com/zepling/hajir/utils/Exts.kt
@@ -1,8 +1,12 @@
 package com.zepling.hajir.utils
 
 import java.time.OffsetDateTime
+import java.time.ZoneId
 import java.time.ZoneOffset
 
 fun OffsetDateTime.convertToNepali():OffsetDateTime{
-    return OffsetDateTime.of(this.toLocalDateTime(), ZoneOffset.of("+05:45"))
+    val odt = OffsetDateTime.of(this.toLocalDateTime(), ZoneOffset.of("+05:45")).atZoneSimilarLocal(ZoneId.of(Keys.ZONE_ID)).toOffsetDateTime()
+
+
+    return odt
 }
